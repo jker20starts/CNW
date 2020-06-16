dfa11b0f6d9317cb243fd9956304ca7fa53098d9
tahuuson <5416784-huusonta99@users.noreply.gitlab.com>
2020-06-16 10:54:14 +0700

    som commit

---
+++ coderush/Areas/TTNHOM_QLTTHPT/Controllers/GiaoVienController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using coderush.Areas.TTNhom_QLTTHPT.Models;
+using OfficeOpenXml;
+
+namespace coderush.Areas.TTNhom_QLTTHPT.Controllers
+{
+    public class GiaoVienController : Controller
+    {
+        QuanLyTTHPTConText db = new QuanLyTTHPTConText();
+
+        // GET: TTNhom_QLTTHPT/GiaoVien
+        public ActionResult Index()
+        {
+            List<GIAOVIEN> res = db.GIAOVIENs.ToList();
+            return View(res);
+        }
+        // GET: TTNhom_QLNS/group_role/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GIAOVIEN diem = db.GIAOVIENs.Find(id);
+            if (diem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(diem);
+        }
+
+        // GET: TTNhom_QLNS/group_role/Create
+        public ActionResult Create()
+        {
+            var gv = db.MONHOCs.ToList();
+            SelectList listMH = new SelectList(gv, "MaMH", "TenMH");
+            ViewBag.CatagoryMH = listMH;
+            return View();
+        }
+
+        // POST: TTNhom_QLNS/group_role/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "HoTenGV,GioiTinh,DiaChi,NgaySinh,SDT,MaHT,MaMH,Luong")] GIAOVIEN dienut)
+        {
+            var gv = db.MONHOCs.ToList();
+            SelectList listMH = new SelectList(gv, "MaMH", "TenMH");
+            ViewBag.CatagoryMH = listMH;
+            if (ModelState.IsValid)
+            {
+                db.GIAOVIENs.Add(dienut);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(dienut);
+        }
+
+        // GET: TTNhom_QLNS/group_role/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            var gv = db.MONHOCs.ToList();
+            SelectList listMH = new SelectList(gv, "MaMH", "TenMH");
+            ViewBag.CatagoryMH = listMH;
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GIAOVIEN diemct = db.GIAOVIENs.Find(id);
+            if (diemct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(diemct);
+        }
+
+        // POST: TTNhom_QLNS/group_role/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaGV,HoTenGV,GioiTinh,NgaySinh,DiaChi,SDT,MaHT,MaMH,Luong")] GIAOVIEN diem)
+        {
+            var gv = db.MONHOCs.ToList();
+            SelectList listMH = new SelectList(gv, "MaMH", "TenMH");
+            ViewBag.CatagoryMH = listMH;
+            if (ModelState.IsValid)
+            {
+                db.Entry(diem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(diem);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            try
+            {
+                GIAOVIEN diem = db.GIAOVIENs.Find(id);
+                db.GIAOVIENs.Remove(diem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch(Exception)
+            {
+                return View("Error");
+            }
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public void ExportToExcel()
+        {
+            List<GIAOVIEN> emplist = db.GIAOVIENs.ToList();
+
+            ExcelPackage pck = new ExcelPackage();
+            ExcelWorksheet ws = pck.Workbook.Worksheets.Add("Report");
+
+            ws.Cells["D1"].Value = "Danh Sách Giáo Viên";
+
+            ws.Cells["C3"].Value = "Ngày Tạo :";
+            ws.Cells["D3"].Value = string.Format("{0:dd MM yyyy} at {0:H: mm tt}", DateTime.Now);
+
+            ws.Cells["A6"].Value = "STT";
+            ws.Cells["B6"].Value = "Họ và Tên";
+            ws.Cells["C6"].Value = "Giới Tính";
+            ws.Cells["D6"].Value = "Ngày Sinh";
+            ws.Cells["E6"].Value = "Địa Chỉ";
+            ws.Cells["F6"].Value = "Số Điện Thoại";
+            ws.Cells["G6"].Value = "Dạy Môn";
+            ws.Cells["H6"].Value = "Lương";
+
+            int rowStart = 7;
+            int stt = 1;
+            foreach (var item in emplist)
+            {
+                ws.Cells[string.Format("A{0}", rowStart)].Value = stt;
+                ws.Cells[string.Format("B{0}", rowStart)].Value = item.HoTenGV;
+                ws.Cells[string.Format("C{0}", rowStart)].Value = item.GioiTinh;
+                ws.Cells[string.Format("D{0}", rowStart)].Value = string.Format("{0:dd/MM/yyyy}", item.NgaySinh);
+                ws.Cells[string.Format("E{0}", rowStart)].Value = item.DiaChi;
+                ws.Cells[string.Format("F{0}", rowStart)].Value = item.SDT;
+                ws.Cells[string.Format("G{0}", rowStart)].Value = item.MONHOC.TenMH;
+                ws.Cells[string.Format("H{0}", rowStart)].Value = item.Luong;
+                rowStart++;
+                stt++;
+            }
+
+
+            ws.Cells["A:AZ"].AutoFitColumns();
+            Response.Clear();
+            Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            Response.AddHeader("content-disposition", "attachment: filename=" + "ExcelReport.xlsx");
+            Response.BinaryWrite(pck.GetAsByteArray());
+            Response.End();
+
+        }
+    }
+}
---
+++ coderush/MVC/Pages.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lte.MVC
+{
+    public static class Pages
+    {
+        public static class Dashboard
+        {
+            public const string dashboard = "/Dashboard/Dashboardv1";
+        }
+        
+        public static class QuanLyTTHPT
+        {
+            public const string Home = "/TTNhom_QLTTHPT/Home/Index";
+            public const string HocSinh = "/TTNhom_QLTTHPT/HocSinh/Index";
+            public const string GiaoVien = "/TTNhom_QLTTHPT/GiaoVien/Index";
+            public const string Diem = "/TTNhom_QLTTHPT/Diem/Index";
+            public const string DiemChiTiet = "/TTNhom_QLTTHPT/DiemChiTiet/Index";
+            public const string MonHoc = "/TTNhom_QLTTHPT/MonHoc/Index";
+            public const string PhanCong = "/TTNhom_QLTTHPT/PhanCong/Index";
+            public const string Lop = "/TTNhom_QLTTHPT/Lop/Index";
+            public const string KhoiLop = "/TTNhom_QLTTHPT/KhoiLop/Index";
+            public const string DienUuTien = "/TTNhom_QLTTHPT/DienUuTien/Index";
+        }
+
+        public static class QLTV
+        {
+            public const string Home = "/TTNhom_QLThuVien/Home/Index";
+            public const string News = "/TTNhom_QLThuVien/News/Index";
+            public const string WorkingSchedule = "/TTNhom_QLThuVien/WorkingSchedule/Index";
+            public const string BorrowGiveBack = "/TTNhom_QLThuVien/BorrowGiveBack/Index";
+            public const string Book = "/TTNhom_QLThuVien/Book/Index";
+            public const string PublishingCompany = "/TTNhom_QLThuVien/PublishingCompany/Index";
+            public const string Author = "/TTNhom_QLThuVien/Author/Index";
+            public const string Category = "/TTNhom_QLThuVien/Category/Index";
+            public const string Readers = "/TTNhom_QLThuVien/Readers/Index";
+            public const string Rules = "/TTNhom_QLThuVien/Rules/Index";
+        }
+        public static class QLKHO
+        {
+            public const string Home = "/TTNhom_QLKHO/Home/Index";
+            public const string News = "/TTNhom_QLKHO/News/Index";
+            public const string HH = "/TTNhom_QLKHO/HH/Index";
+            public const string KH = "/TTNhom_QLKHO/KH/Index";
+            public const string DV = "/TTNhom_QLKHO/DV/Index";
+            public const string NCC = "/TTNhom_QLKHO/NCC/Index";
+            public const string PN = "/TTNhom_QLKHO/PN/Index";
+            public const string TTPN = "/TTNhom_QLKHO/TTPN/Index";
+            public const string PX = "/TTNhom_QLKHO/PX/Index";
+            public const string TTPX = "/TTNhom_QLKHO/TTPX/Index";
+        }
+
+
+        public static class GroupRole
+        {
+            public const string Index = "/TTNhom_QLNS/group_role/Index";
+            public const string Create = "/TTNhom_QLNS/group_role/Create";
+            public const string Detail = "/TTNhom_QLNS/group_role/Details";
+            public const string Update = "/TTNhom_QLNS/group_role/Edit";
+            
+        }
+        public static class Department
+        {
+            public const string Index = "/TTNhom_QLNS/Departments/Index";
+            public const string Create = "/TTNhom_QLNS/Departments/Create";
+            public const string Detail = "/TTNhom_QLNS/Departments/Details";
+            public const string Update = "/TTNhom_QLNS/Departments/Edit";
+        }
+        public static class Position
+        {
+            public const string Index = "/TTNhom_QLNS/Positions/Index";
+            public const string Create = "/TTNhom_QLNS/Positions/Create";
+            public const string Detail = "/TTNhom_QLNS/Positions/Details";
+            public const string Update = "/TTNhom_QLNS/Positions/Edit";
+        }
+        public static class Staff
+        {
+            public const string Index = "/TTNhom_QLNS/Staffs/Index";
+            public const string Create = "/TTNhom_QLNS/Staffs/Create";
+            public const string Detail = "/TTNhom_QLNS/Staffs/Details";
+            public const string Update = "/TTNhom_QLNS/Staffs/Edit";
+        }
+
+        public static class QLKS
+        {
+            public const string Home = "/TTNhom_QLKS/Home/Index";
+            public const string Employees = "/TTNhom_QLKS/NhanViens/Index";
+            public const string Payments = "/TTNhom_QLKS/PhieuChis/Index";
+            public const string Clients = "/TTNhom_QLKS/KhachHangs/Index";
+            public const string GeneralService = "/TTNhom_QLKS/DichVuChungs/Index";
+            public const string Rooms = "/TTNhom_QLKS/Phongs/Index";
+            public const string Receipts = "/TTNhom_QLKS/PhieuThus/Index";
+        }
+        public static class Dashbaordv2
+        {
+            public const string Url = "/Dashboard/Dashboardv1";
+        }
+
+        public static class LayoutTop
+        {
+            public const string Url = "/Layout/Top";
+        }
+
+        public static class LayoutBoxed
+        {
+            public const string Url = "/Layout/Boxed";
+        }
+
+        public static class LayoutFixed
+        {
+            public const string Url = "/Layout/Fixed";
+        }
+
+        public static class LayoutCollapsed
+        {
+            public const string Url = "/Layout/Collapsed";
+        }
+
+        public static class WidgetIndex
+        {
+            public const string Url = "/Widget/Index";
+        }
+
+        public static class ChartChartJS
+        {
+            public const string Url = "/Chart/ChartJS";
+        }
+
+        public static class ChartMorris
+        {
+            public const string Url = "/Chart/Morris";
+        }
+
+        public static class ChartFlot
+        {
+            public const string Url = "/Chart/Flot";
+        }
+
+        public static class ChartInline
+        {
+            public const string Url = "/Chart/Inline";
+        }
+
+        public static class UIElementGeneral
+        {
+            public const string Url = "/UIElement/General";
+        }
+
+        public static class UIElementIcon
+        {
+            public const string Url = "/UIElement/Icon";
+        }
+
+        public static class UIElementButton
+        {
+            public const string Url = "/UIElement/Button";
+        }
+
+        public static class UIElementSlider
+        {
+            public const string Url = "/UIElement/Slider";
+        }
+
+        public static class UIElementTimeline
+        {
+            public const string Url = "/UIElement/Timeline";
+        }
+
+        public static class UIElementModal
+        {
+            public const string Url = "/UIElement/Modal";
+        }
+
+        public static class FormGeneral
+        {
+            public const string Url = "/Form/General";
+        }
+
+        public static class FormAdvanced
+        {
+            public const string Url = "/Form/Advanced";
+        }
+
+        public static class FormEditor
+        {
+            public const string Url = "/Form/Editor";
+        }
+
+        public static class TableSimple
+        {
+            public const string Url = "/Table/Simple";
+        }
+
+        public static class TableData
+        {
+            public const string Url = "/Table/Data";
+        }
+
+        public static class CalendarIndex
+        {
+            public const string Url = "/Calendar/Index";
+        }
+
+        public static class MailboxIndex
+        {
+            public const string Url = "/Mailbox/Index";
+        }
+
+        public static class MailboxCompose
+        {
+            public const string Url = "/Mailbox/Compose";
+        }
+
+        public static class MailboxRead
+        {
+            public const string Url = "/Mailbox/Read";
+        }
+
+        public static class ExampleInvoice
+        {
+            public const string Url = "/Example/Invoice";
+        }
+
+        public static class ExampleInvoicePrint
+        {
+            public const string Url = "/Example/InvoicePrint";
+        }
+
+        public static class ExampleProfile
+        {
+            public const string Url = "/Example/Profile";
+        }
+
+        public static class ExampleLogin
+        {
+            public const string Url = "/Example/Login";
+        }
+
+        public static class ExampleRegister
+        {
+            public const string Url = "/Example/Register";
+        }
+
+        public static class ExampleLockscreen
+        {
+            public const string Url = "/Example/Lockscreen";
+        }
+
+        public static class ExampleError404
+        {
+            public const string Url = "/Example/Error404";
+        }
+
+        public static class ExampleError500
+        {
+            public const string Url = "/Example/Error500";
+        }
+
+        public static class ExampleBlankPage
+        {
+            public const string Url = "/Example/BlankPage";
+        }
+
+        public static class ExamplePacePage
+        {
+            public const string Url = "/Example/PacePage";
+        }
+        public static class Account
+        {
+            public const string Logout = "/Account/LogOff";
+        }
+    }
+}
